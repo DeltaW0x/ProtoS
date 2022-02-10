0231fb0a817b0f07b48344a3c5e641fdbe9e9ca3
Luca Cassarino <deltawaveanimations@gmail.com>
2022-02-10 22:27:57 +0000

    Added Maw Example

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,31 +1,28 @@
-
-#include "MaxMatrix/MaxMatrix.hpp"
 #include "mbed.h"
 #include <cstdint>
+#include <MaxMatrix/MaxMatrix.hpp>
 
-MaxMatrix *matrix;
 
-const uint8_t mawL[] = {32,         8,          0b00000000, 0b00000000, 0b00000000, 0b00000111, 0b00011111,
-                        0b01111000, 0b11100000, 0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
-                        0b11100000, 0b01111000, 0b00011110, 0b00000111, 0b00000000, 0b00000000, 0b00000000,
-                        0b00000001, 0b00000111, 0b00011110, 0b01111000, 0b11100000, 0b00000100, 0b00011110,
-                        0b01111011, 0b11100011, 0b11111111, 0b00000000, 0b00000000, 0b00000000};
-
-int main()
-{
-    PinName CS = D6;
-    PinName DIN = D7;
-    PinName CLK = D5;
-
-    uint8_t maxInUse = 4;
-
-    matrix = new MaxMatrix(DIN,CS,CLK,maxInUse);
-
-    matrix->init();
-    matrix->clear();
-    matrix->setIntensity(3);
-    matrix->clear();
-
+const uint8_t mawL[] =      {32, 8,
+                        0b00000000, 0b00000000, 0b00000000, 0b00000111, 0b00011111, 0b01111000, 0b11100000, 0b10000000,
+                        0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b11100000, 0b01111000, 0b00011110, 0b00000111,
+                        0b00000000, 0b00000000, 0b00000000, 0b00000001, 0b00000111, 0b00011110, 0b01111000, 0b11100000,
+                        0b00000100, 0b00011110, 0b01111011, 0b11100011, 0b11111111, 0b00000000, 0b00000000, 0b00000000
+                       };
     
-    matrix->writeSprite(0,0,mawL);
+
+
+
+
+int main(){
+
+    MaxMatrix* m = new MaxMatrix(A2,A1,A0,4);
+
+    m->init();
+    m->setIntensity(8);
+    m->clear();
+
+    while(true){
+        m->writeSprite(0,0,mawL);
+    }
 }
