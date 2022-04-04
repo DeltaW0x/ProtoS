1c33036569ed6e3729fc81296a237beee792fe7b
Delta <deltawaveanimations@gmail.com>
2022-04-04 10:08:45 +0200

    Code Cleanup

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,11 +1,17 @@
+#include "PinNames.h"
 #include "mbed.h"
 #include <MaxMatrix/MaxMatrix.hpp>
-#include <chrono>
 #include <cstdint>
 
 
+PinName DIN = A7;
+PinName CS = A5;
+PinName CLK = A6;
+
+uint8_t MAX_N = 2;
 
 int main()
 {
-    MaxMatrix *m = new MaxMatrix(A7, A5, A6, 2);
+    MaxMatrix *m = new MaxMatrix(DIN, CS, CLK, MAX_N);
+    
 }
