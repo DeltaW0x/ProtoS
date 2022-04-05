3b0b56acc9184260d59d8cfb0f1300e3ef34ab63
Delta <deltawaveanimations@gmail.com>
2022-04-05 22:44:59 +0200

    updated main()

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,6 +5,7 @@
 
 #include "MaxMatrix/MaxMatrix.hpp"
 #include "Core/PPU.hpp"
+#include "Core/SPI_BUS.hpp"
 
 PinName DIN = A7;
 PinName CS = A5;
@@ -13,8 +14,10 @@ PinName CLK = A6;
 uint8_t MAX_N = 2;
 
 PPU* PPU::instance = nullptr;
-std::once_flag PPU::initInstanceFlag;
+SPI_BUS* SPI_BUS::instance = nullptr;
 
+std::once_flag PPU::initInstanceFlag;
+std::once_flag SPI_BUS::initInstanceFlag;
 
 int main()
 {
